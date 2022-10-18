f322f7cf3327f617374d365fe709029458af3d40
kavyahallur123 <kavyahallur199@gmail.com>
2022-10-18 12:31:39 +0530

    Welcome to generics program

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+package com.bridgelabzGenerics;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Generics to problem to find max values");
+    }
+}
