a616544784fa31060103d3f13e9f67059ad5d74c
kavyahallur123 <kavyahallur199@gmail.com>
2023-02-24 02:53:52 +0530

    Addedd: FInd the maximum float

--- FindMaximum.java
+++ FindMaximum.java
@@ -2,9 +2,20 @@ package com.bridgelabz;
 
 public class FindMaximum {
 
-    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3) {
 
-        T max = a1;
+        Integer max = a1;
+
+        if (a2.compareTo(max) > 0)
+            max = a2;
+
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
+    public static Float maxOfFloat(Float a1, Float a2, Float a3) {
+
+        Float max = a1;
 
         if (a2.compareTo(max) > 0)
             max = a2;
@@ -15,9 +26,10 @@ public class FindMaximum {
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome To Find Maximun Problem Using Generics \n");
-        System.out.println("The maximum value between the three integer is: " + maxOfValues(3, 8, 5) + "\n");
-        System.out.println("The maximum value between the three float is: " + maxOfValues(1.5f, 3.56f, 5.87f) + "\n");
-        System.out.println("The maximum value between the three string is: " + maxOfValues("abc", "pqr", "xyz"));
+
+        System.out.println("Welcome To Find Maximum Problem Using Generics\n");
+        System.out.println("The maximum value between the three integer is: " + maxOfInteger(3, 8, 5));
+        System.out.println("------------------------------------------------");
+        System.out.println("The maximum value between the three float is: " + maxOfFloat(3.12F, 8.14F, 5.32F));
     }
 }
